23728d402c04a167bd63d4a82b1bf601b6439bd4
that-ambuj <ambujs89@gmail.com>
2023-04-01 17:24:39 +0530

    change color scheme to gruvbuddy

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,10 +1,12 @@
 vim.opt.winblend = 0
 vim.opt.pumblend = 0
 
-require("vitesse").setup {
-  comment_italics = true,
-  transparent_background = true,
-  transparent_float_background = true, -- aka pum(popup menu) background
-  reverse_visual = false,
-  dim_nc = false,
-}
+require("colorbuddy").colorscheme("gruvbuddy")
+
+-- require("vitesse").setup {
+--   comment_italics = true,
+--   transparent_background = true,
+--   transparent_float_background = true, -- aka pum(popup menu) background
+--   reverse_visual = false,
+--   dim_nc = false,
+-- }
