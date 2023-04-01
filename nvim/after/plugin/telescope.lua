9d1c272aea204e6ecac525c4ccdce051f801b727
that-ambuj <ambujs89@gmail.com>
2023-04-01 12:35:14 +0530

    configure lazygit telescope plugin

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,8 +1,14 @@
 local builtin = require('telescope.builtin')
 
+-- Load lazygit extension for telescope
+require('telescope').load_extension('lazygit')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+
+-- Enable telescore lazygit plugin
+vim.keymap.set('n', '<leader>tg', require("telescope").extensions.lazygit.lazygit)
 
 -- vim.keymap.set('n', '<leader>ps', function()
 	-- builtin.grep_string({ search = vim.fn.input("Grep :> ") })
