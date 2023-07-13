a7d8fe7b14bf1525eae1be5608e37bdcba8c7af2
Ambuj Singh <ambujs89@gmail.com>
2023-07-13 23:30:25 +0530

    feat(neovide): configure neovide font

--- nvim/lua/ambuj/init.lua
+++ nvim/lua/ambuj/init.lua
@@ -1,1 +1,6 @@
-if vim.g.neovide then vim.o.guifont = "JetBrains Mono" end
+if vim.g.neovide then
+    vim.o.guifont = "SF Mono:h15"
+    -- vim.g.neovide_refresh_rate = 144
+    vim.opt.linespace = 4
+    vim.g.neovide_cursor_animation_length = 0.13
+end
