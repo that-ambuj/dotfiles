75822b45548b87e18b4f09d780b212cc87a37a66
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-24 23:21:32 +0530

    deps: update treesitter version

--- nvim/lua/ambuj/packer.lua
+++ nvim/lua/ambuj/packer.lua
@@ -91,7 +91,7 @@ return require("packer").startup(function(use)
 
     use("folke/todo-comments.nvim")
 
-    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate", tag = "0.9.0" })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate", tag = "0.9.1" })
     use("nvim-treesitter/nvim-treesitter-context")
     use("theprimeagen/harpoon")
     use("mbbill/undotree")
