de821368dd3671f1d192b6a77923911a17e4be28
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-01-22 10:44:05 +0530

    feat: enabler gitblame

--- nvim/after/plugin/gitblame.lua
+++ nvim/after/plugin/gitblame.lua
@@ -1,4 +1,8 @@
-vim.cmd("GitBlameDisable")
+-- vim.cmd("GitBlameDisable")
+require("gitblame").setup {
+    enabled = true,
+    ignored_filetypes = { "netrw" },
+}
 
 vim.keymap.set("n", "<leader>bg", "<Cmd>GitBlameToggle<CR>", {
     desc = "Toggle Git Line Blame"
