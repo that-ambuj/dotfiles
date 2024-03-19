eea307726506a5dee76ced1fcc8a93fdbd221b45
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-03-19 17:51:01 +0530

    remove rest.nvim

--- nvim/after/plugin/rest.lua
+++
@@ -1,5 +0,0 @@
-require("rest-nvim").setup({})
-
-vim.keymap.set("n", "<leader>rn", "<Plug>RestNvim", { desc = "Run Request under the cursor" });
-vim.keymap.set("n", "<leader>rp", "<Plug>RestNvimPreview", { desc = "Preview the request cURL Command" })
-vim.keymap.set("n", "<leader>rl", "<Plug>RestNvimLast", { desc = "re-run the last request" })
