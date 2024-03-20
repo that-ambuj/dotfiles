45db8e74420592edbe37af1aba0b9f6bc2c2bfad
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-03-20 14:43:42 +0530

    add blade queries

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -38,7 +38,7 @@ require('lazy').setup {
 
   require 'kickstart/plugins/treesitter',
 
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/lua/scripts/blade.lua
@@ -0,0 +1,14 @@
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = 'https://github.com/EmranMR/tree-sitter-blade',
+    files = { 'src/parser.c' },
+    branch = 'main',
+  },
+  filetype = 'blade',
+}
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  pattern = '*.blade.php',
+  command = 'set ft=blade',
+})
