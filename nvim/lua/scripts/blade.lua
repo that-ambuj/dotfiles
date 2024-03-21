f3b88fe4b34de375b2f42335d4f89596b59b010b
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-03-21 14:15:53 +0530

    pin the version for tree-sitter-blade

--- nvim/lua/scripts/blade.lua
+++ nvim/lua/scripts/blade.lua
@@ -1,14 +1,21 @@
 local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
 parser_config.blade = {
   install_info = {
     url = 'https://github.com/EmranMR/tree-sitter-blade',
     files = { 'src/parser.c' },
-    branch = 'main',
+    branch = 'v0.10.0',
   },
   filetype = 'blade',
 }
 
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  pattern = '*.blade.php',
-  command = 'set ft=blade',
-})
+-- vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+--   pattern = '*.blade.php',
+--   command = 'set ft=blade',
+-- })
+
+vim.filetype.add {
+  pattern = {
+    ['.*%.blade%.php'] = 'blade',
+  },
+}
