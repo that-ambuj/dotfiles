3c1848ac2a1ad8f46107a2b6fcae00de5d9c9137
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-03-21 15:19:53 +0530

    add query injections for php attrbutes

--- nvim/after/queries/html/injections.scm
+++ nvim/after/queries/html/injections.scm
@@ -15,3 +15,16 @@
 ; ((style_element
 ;   (raw_text) @injection.content)
 ;  (#set! injection.language "css"))
+
+; Blade PHP attributes
+; <x-foo :bar="$baz" />
+(element
+  (_
+    (tag_name) @_tag
+      (#lua-match? @_tag "^x%-%l")
+    (attribute
+      (attribute_name) @_attr
+        (#lua-match? @_attr "^:%l")
+      (quoted_attribute_value
+        (attribute_value) @injection.content)
+      (#set! injection.language "php_only"))))
