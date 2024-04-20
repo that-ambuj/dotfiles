1d6d92ca3eec4885eed83565fe0dde2045239828
Ambuj Kumar <ambujs89@gmail.com>
2024-04-21 03:40:02 +0530

    format js and ts files with prettier

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -16,7 +16,10 @@ return {
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
       },
     },
   },
