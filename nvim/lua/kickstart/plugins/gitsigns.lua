a347b64e7b0fc67ee5b64425fede5b91d75100a2
Ambuj Kumar <ambujs89@gmail.com>
2024-05-15 12:24:12 +0530

    enable gitsigns

--- nvim/lua/kickstart/plugins/gitsigns.lua
+++ nvim/lua/kickstart/plugins/gitsigns.lua
@@ -6,7 +6,7 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    enabled = false,
+    enabled = true,
     opts = {
       signs = {
         add = { text = '+' },
