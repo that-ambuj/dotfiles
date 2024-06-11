c06bd4b54ac46b6f9f20f9f07a5401bb3d48d72e
Ambuj Kumar <ambujs89@gmail.com>
2024-06-11 16:33:45 +0530

    configure typescript.nvim

---
+++ nvim/lua/custom/plugins/typescript.lua
@@ -0,0 +1,35 @@
+return {
+  'jose-elias-alvarez/typescript.nvim',
+  opts = {
+    server = {
+      on_attach = function(client, bufnr)
+        -- vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+      end,
+      settings = {
+        -- specify some or all of the following settings if you want to adjust the default behavior
+        javascript = {
+          inlayHints = {
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
+          },
+        },
+        typescript = {
+          inlayHints = {
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
+          },
+        },
+      },
+    },
+  },
+}
